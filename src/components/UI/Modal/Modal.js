827e427e17e06cffb11d581882dbbe9bd8df4477
priya.verma <priya.verma@favcy.in>
2020-03-05 15:47:01 +0530

    add burger builder project connected thorugh backend

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,7 +4,7 @@ import BackDrop from "../BackDrop/BackDrop";
 import Aux from "../../../hoc/Aux/Aux";
 class Modal extends Component{
     shouldComponentUpdate(nextProps,nextState){
-        return nextProps.show !== this.props.show
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children
     }
     componentWillUpdate () {
         console.log("priya componentWillUpdate")
---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Spinner.css";
+
+const spinner =() =>{
+    return(
+        <div className="Loader">Loading...</div>
+    )
+}
+export default spinner;
